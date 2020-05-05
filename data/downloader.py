ef609a9ae1f9e8d1bdc792e330f010eb2fb7d5a2
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2020-05-05 20:41:23 +0900

    add coco dataset in downloader

--- data/downloader.py
+++ data/downloader.py
@@ -1,16 +1,21 @@
-__all__ = ['voc2007_trainval', 'voc2007_test', 'voc2012_trainval', 'voc2012_test']
-
+choices = ['voc2007_trainval', 'voc2007_test', 'voc2012_trainval', 'voc2012_test', 'coco2014_trainval', 'coco2014_test']
+__all__ = choices
 import os
 import pycurl
-import tarfile
+import tarfile, zipfile
 import glob
 import logging
 
 class _Downloader:
-    def __init__(self, url):
+    def __init__(self, url, compress_ext='tar'):
         self.url = url
 
-    def run(self, out_base_dir, dirname):
+        _compress_exts = ['tar', 'zip']
+        if not compress_ext in _compress_exts:
+            raise ValueError("Invalid argument, select proper extension from {}, but got {}".format(_compress_exts, compress_ext))
+        self.compress_ext = compress_ext
+
+    def run(self, out_base_dir, dirname, remove_comp_file=True):
         out_dir = os.path.join(out_base_dir, dirname)
 
         if len(glob.glob(os.path.join(out_dir, '*'))) > 0:
@@ -26,21 +31,28 @@ class _Downloader:
 
         os.makedirs(out_dir, exist_ok=True)
 
-        tarpath = os.path.join(out_base_dir, 'tmp.tar')
+        dstpath = os.path.join(out_base_dir, 'tmp.{}'.format(self.compress_ext))
 
-        with open(tarpath, 'wb') as f:
+        with open(dstpath, 'wb') as f:
             curl.setopt(pycurl.WRITEFUNCTION, f.write)
             curl.perform()
 
         curl.close()
 
 
-        # extract tar
-        with tarfile.open(tarpath) as tar:
-            tar.extractall(out_dir)
+        # extract
+        if self.compress_ext == 'tar':
+            with tarfile.open(dstpath) as tar_f:
+                tar_f.extractall(out_dir)
+        elif self.compress_ext == 'zip':
+            with zipfile.ZipFile(dstpath, 'r') as zip_f:
+                zip_f.extractall(out_dir)
+        else:
+            assert False, "Bug occurred"
 
-        # remove tmp.tar
-        os.remove(tarpath)
+        if remove_comp_file:
+            # remove tmp.*
+            os.remove(dstpath)
 
 
 from .utils import DATA_ROOT
@@ -61,3 +73,23 @@ def voc2012_test():
     test_downloader = _Downloader('http://pjreddie.com/media/files/VOC2012test.tar')
     test_downloader.run(DATA_ROOT + '/voc/voc2012', 'test')
 
+def coco2014_trainval():
+    # get images
+    train_downloader = _Downloader('http://images.cocodataset.org/zips/train2014.zip', 'zip')
+    train_downloader.run(DATA_ROOT + '/coco/coco2014/images', 'train', remove_comp_file=False)
+
+    val_downloader = _Downloader('http://images.cocodataset.org/zips/val2014.zip', 'zip')
+    val_downloader.run(DATA_ROOT + '/coco/coco2014/images', 'val', remove_comp_file=False)
+
+    # annotations
+    trainval_downloader = _Downloader('http://images.cocodataset.org/annotations/annotations_trainval2014.zip', 'zip')
+    trainval_downloader.run(DATA_ROOT + '/coco/coco2014/annotations', 'train', remove_comp_file=False)
+
+def coco2014_test():
+    # get images
+    test_downloader = _Downloader('http://images.cocodataset.org/zips/test2014.zip', 'zip')
+    test_downloader.run(DATA_ROOT + '/coco/coco2014/images', 'test', remove_comp_file=False)
+
+    # annotations
+    test_downloader = _Downloader('http://images.cocodataset.org/annotations/annotations_trainval2014.zip', 'zip')
+    test_downloader.run(DATA_ROOT + '/coco/coco2014/annotations', 'test', remove_comp_file=False)
--- get_dataset.py
+++ get_dataset.py
@@ -1,8 +1,23 @@
 from data.downloader import *
+from data.downloader import choices
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Download datasets')
+parser.add_argument('--datasets', help='select datasets from {}'.format(choices),
+                    choices=choices, nargs='*', required=True)
+args = parser.parse_args()
 
 if __name__ == '__main__':
-    #voc2007_trainval()
-    #voc2007_test()
-    #voc2012_trainval()
-    voc2012_test()
-    pass
+    if 'voc2007_trainval' in args.datasets:
+        voc2007_trainval()
+    if 'voc2007_test' in args.datasets:
+        voc2007_test()
+    if 'voc2012_trainval' in args.datasets:
+        voc2012_trainval()
+    if 'voc2012_test' in args.datasets:
+        voc2012_test()
+    if 'coco2014_trainval' in args.datasets:
+        coco2014_trainval()
+    if 'coco2014_test' in args.datasets:
+        coco2014_test()
